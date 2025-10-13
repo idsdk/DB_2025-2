c566f39b879785f13828fa94eae6079615a89bb1
DB_2025 <idsdkk@gmail.com>
2025-10-13 16:44:00 +0900

    db

---
+++ src/mvc_jdbc_test/controller/entity/Customer.java
@@ -0,0 +1,58 @@
+package mvc_jdbc_test.controller.entity;
+
+public class Customer {
+    private String  customer_id;
+    private String customer_name;
+    private int age;
+    private String level;
+    private String job;
+    private int reward; // 적립금
+
+    public String getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(String customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getCustomer_name() {
+        return customer_name;
+    }
+
+    public void setCustomer_name(String customer_name) {
+        this.customer_name = customer_name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+}
---
+++ src/mvc_jdbc_test/controller/view/CustomerView.java
@@ -0,0 +1,4 @@
+package mvc_jdbc_test.controller.view;
+
+public class CustomerView {
+}
